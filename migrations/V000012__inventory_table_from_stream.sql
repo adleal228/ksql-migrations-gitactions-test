686626c8a4aae1782680b67ac7b3b195c7469999
Adrian Leal <adleal@confluent.io>
2022-10-20 15:04:46 -0500

    git action to create table from stream window hoping group by id

--- migrations/V000012__inventory_table_from_stream.sql
+++ migrations/V000012__inventory_table_from_stream.sql
@@ -3,10 +3,10 @@ CREATE TABLE INVENTORY_TABLE_FROM_STREAM_58_V1 AS
     id AS k1,
     quantity AS k2,
     productid AS k3,
-    AS_VALUE(id) AS rep_id,
+    AS_VALUE(id) AS id,
     WINDOWSTART AS EVENT_TS,
-    AS_VALUE(quantity) AS rep_quantity,
-    AS_VALUE(productid) AS rep_productid,
+    AS_VALUE(quantity) AS quantity,
+    AS_VALUE(productid) AS productid,
     COUNT (*) AS events
   FROM INVENTORY_58_V2 WINDOW TUMBLING (SIZE 10 SECOND)
   GROUP BY id, quantity, productid;
